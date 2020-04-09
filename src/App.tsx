d6ee11cd026227b3de5460e3e360965b1500a1cb
Lucas Mallmann <lucasmallmann76@gmail.com>
2020-04-08 23:05:44 -0300

    feat: Added homescreen with styled component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 
 import Home from '~/Home';
 
 const App = () => {
-  return <Home />;
+  return (
+    <>
+      <StatusBar backgroundColor="#6874e8" barStyle="light-content" />
+      <Home />
+    </>
+  );
 };
 
 export default App;
--- src/Home/styles.ts
+++ src/Home/styles.ts
@@ -3,9 +3,13 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
   flex: 1;
   background-color: #6874e8;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const Greeting = styled.Text`
   color: #f4f4ed;
   font-weight: bold;
+  font-size: 24px;
+  text-align: center;
 `;
