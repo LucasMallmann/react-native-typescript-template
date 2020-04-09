caf1249a46688dad05f82de086897e82720680f3
Lucas Mallmann <lucasmallmann76@gmail.com>
2020-04-08 22:19:16 -0300

    chore: eslint and typescript import resolver

---
+++ src/Home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container, Greeting } from './styles';
+
+export default function Home() {
+  return (
+    <Container>
+      <Greeting>Welcome to the Typescript Template</Greeting>
+    </Container>
+  );
+}
---
+++ src/Home/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #6874e8;
+`;
+
+export const Greeting = styled.Text`
+  color: #f4f4ed;
+  font-weight: bold;
+`;
